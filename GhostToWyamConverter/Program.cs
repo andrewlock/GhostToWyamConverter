f4ba3332c2edd8aa4bf53f718d5282b5126d11a8
Andrew Lock <andrewlock.net@gmail.com>
2017-08-26 00:25:12 +0100

    Add project files.

---
+++ GhostToWyamConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GhostToWyamConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
